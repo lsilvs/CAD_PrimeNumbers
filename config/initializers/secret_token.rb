bc4e99d0bf9b0d2d6294e621e3f6ac691945d710
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-02-20 13:52:47 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Primenumbers::Application.config.secret_token = 'c1bd6953b9ecfc86a1c8fbdea336cc16f1ab004f51dff7a6059d9b16b5329d3d8854e0c496c4b1b8267788005cacdf8e82f01b6e1e2d7a63393bcc375feb9e9f'
